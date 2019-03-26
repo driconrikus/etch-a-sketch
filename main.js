d4b25273ca378d6c5cc7b10b0168a159c6109d67
Ricardo Valdez <rickondrius@gmail.com>
2019-03-26 01:11:57 -0400

    Added files, still needs some work.

---
+++ main.js
@@ -0,0 +1,54 @@
+const gridSquare = document.querySelector('.grid');
+let out = document.querySelector('#xrange');
+let gridSlider = document.querySelector('#range');
+out.value = gridSlider.value;
+let btn = document.querySelector('#newgrid');
+let rainbowBtn = document.querySelector('#rainbow');
+btn.addEventListener('click', ask);
+rainbowBtn.addEventListener('click', rainbowColors);
+let colorCnt = 0;
+
+// Slider input for changing the grid size.
+gridSlider.oninput = function(){
+    out.value = this.value;
+}
+
+// This resets the grid for resizing.
+function reset(){
+    while(gridSquare.firstChild){
+        gridSquare.removeChild(gridSquare.firstChild);
+    }
+}
+
+// Grid creation dialog.
+function ask(){
+    let ask = confirm(`Create new grid with ${out.value}x${out.value} squares?`);
+    reset();
+    if (ask){
+        createGrid(out.value);
+    }
+}
+
+// This function creates the grid as well as filling it with black on mouseover.
+function createGrid(size){
+    gridSquare.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridSquare.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for(i=0; i<size*size; i++){
+        const div = document.createElement('div');
+        div.addEventListener('mouseover', fillBlack);
+        div.classList.add('square');
+        gridSquare.appendChild(div);
+        colorCnt = 0;
+    }
+}
+
+// Fills the grid with black. 
+function fillBlack(){
+    this.style.backgroundColor = "#000000";
+    colorCnt += 0.10;
+  }
+
+// Fills the grid with rainbow colors!
+function rainbowColors(){
+      alert('Coming soon!');
+  }
